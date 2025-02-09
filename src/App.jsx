f39f6dfbc074b93f3120b5e30e7fee34193e6807
Dor Katzir <katzirdor@gmail.com>
2025-02-10 00:40:57 +0200

    @utility directive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-deepblue p-12'>
+    <div className='bg-deepblue p-12 container'>
       <h1 className='text-5xl font-bold text-white'>
         This is a React app with Tailwind CSS v4.0
       </h1>
