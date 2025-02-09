16b5dc1463c6806bd2141c4e83da3ce8a36341c7
Dor Katzir <katzirdor@gmail.com>
2025-02-09 20:33:44 +0200

    custom color classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-500 p-12'>
+    <div className='bg-deepblue p-12'>
       <h1 className='text-5xl font-bold text-white'>
         This is a React app with Tailwind CSS v4.0
       </h1>
