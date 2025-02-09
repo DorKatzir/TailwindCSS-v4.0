63a4e474712604e87e78a3aeb4680d78f847b9a0
Dor Katzir <katzirdor@gmail.com>
2025-02-09 20:22:50 +0200

    vite ans tw v4 installation and configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-500 p-12'>
+      <h1 className='text-5xl font-bold text-white'>
+        This is a React app with Tailwind CSS v4.0
+      </h1>
+    </div>
+  )
+}
+
+export default App
